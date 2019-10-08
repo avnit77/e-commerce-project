f8aee0f5435e04efbb32e0008d977260a91c179d
Tali Avni <tali@aristo7.com>
2019-10-07 17:14:13 -0700

    first product

---
+++ data/rocks.js
@@ -0,0 +1,19 @@
+const igneous = {
+    id: 'igneous',
+    name: 'Igneous Rock',
+    image: '../photos/igneous.jpg',
+    description: 'Volcano barf',
+    category: 'rocks with holes',
+    price: 1.00,
+};
+
+const anthracite = {
+    id: 'metamorphic',
+    name: 'Shiny Coal',
+    image: '../photos/metamorphic.jpg',
+    description: 'The fanciest coal',
+    category: 'tropical',
+    price: 20.00,
+};
+
+const 
