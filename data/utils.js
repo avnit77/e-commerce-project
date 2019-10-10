8876993f19086a194f3d073e52ac3b49767a6fd0
Tali Avni <tali@aristo7.com>
2019-10-10 09:08:27 -0700

    version 1, can't figure it out

--- data/utils.js
+++ data/utils.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import rocks from '../api.js';
 export const makePrettyCurrency = (number) =>
     number
@@ -27,19 +28,19 @@ export function calcOrderTotal(cart, rocks) {
 
     let orderTotal = 0;
 
-    for (let i = 0; i < cart.length; i++) {
-        const lineItem = cart[i];
+    // for (let i = 0; i < cart.length; i++) {
+    //     const lineItem = cart[i];
+    //     const rock = findById(rocks, lineItem.id);
+    //     const lineTotal = calcLineTotal(lineItem.quantity, rock.price);
+    //     orderTotal += lineTotal;
+    // }
+
+    // could also handle with forEach:
+    cart.forEach(lineItem => {
         const rock = findById(rocks, lineItem.id);
         const lineTotal = calcLineTotal(lineItem.quantity, rock.price);
         orderTotal += lineTotal;
-    }
-
-    //could also handle with forEach:
-    // cart.forEach(lineItem => {
-    //     cont rock = findById(product, lineItem.id);
-    //     const lineTotal = calcLineItem(lineItem.quantity, plant.price);
-    //     orderTotal+=lineTotal
-    // });
+    });
 
     return makePrettyCurrency(orderTotal);
 }
--- shopping-cart/app.js
+++ shopping-cart/app.js
@@ -1,9 +1,11 @@
 import renderTableRow from './render-table-row.js';
 import rocks, { cart } from '../api.js';
 import { findById, makePrettyCurrency, calcOrderTotal } from '../data/utils.js';
+import { CART_KEY } from '../products/render-rocks.js';
 
 const tableElement = document.querySelector('tbody');
-const totalCell = document.getElementById('order-total-cell');
+const orderTotalCell = document.getElementById('order-total-cell');
+
 
 
 for (let i = 0; i < cart.length; i++) {
@@ -15,5 +17,34 @@ for (let i = 0; i < cart.length; i++) {
 }
 
 let cartTotal = calcOrderTotal(cart, rocks);
-totalCell.textContent = makePrettyCurrency(cartTotal);
+orderTotalCell.textContent = makePrettyCurrency(cartTotal);
+
+const addRow = (rockOrder, rocks) => {
+    const orderrock = findById(rockOrder.id, rocks);
+    const row = renderTableRow(orderrock, rockOrder);
+
+    tableElement.appendChild(row);
+};
+
+const addRows = (cart, rocks) => {
+    cart.forEach(rockOrder => {
+        addRow(rockOrder, rocks);
+    });
+};
+
+const buildTotalCell = (cart, rocks) => {
+    const totalCell = document.getElementById('order-total-cell');
+    const cartTotal = calcOrderTotal(cart, rocks);
+
+    totalCell.textContent = makePrettyCurrency(cartTotal);
+};
+
+const buildTable = (cart, rocks) => {
+    buildTotalCell(cart, rocks);
+    addRows(cart, rocks);
+};
+
+const javascriptReadableCart = JSON.parse(localStorage.getItem(CART_KEY));
+debugger;
+buildTable(javascriptReadableCart, dataRocks);
     
